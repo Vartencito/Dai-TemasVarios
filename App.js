641609f27444193524503212bd6bfea560854cf1
Valen <vatenenberg@gmail.com>
2022-10-03 22:07:05 -0300

    hecho la flatlist con contactos

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import * as Contacts from "expo-contacts";
 
 export default function App() {
+  const [contacts, setContacts] = useState([]);
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === "granted") {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers],
+      });
+      if (data.length > 0) {
+        setContacts(data);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getContacts();
+  }, []);
+
+  console.log('contactos',contacts);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={contacts}
+        renderItem={({ item }) => (
+          <View style={styles.contenedor}>
+            <Text>nombre: {item.name}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +43,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  contenedor: {
+    backgroundColor: "red",
+    width: 200,
+    height: 'auto',
+    margin: 10,
   },
 });
